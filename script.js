74e2c98689649363bbe6b91b801baccfc214aa41
Guru Darji <gurudarji@ymail.com>
2022-03-01 18:25:30 -0500

    made the skeleton of the questions

---
+++ script.js
@@ -0,0 +1,37 @@
+// var startEl = document.querySelector('#start');
+// var beginBtn = document.querySelector('#begin');
+// var gameEl = document.querySelector('#game');
+// var endEl = document.querySelector('#end');
+// var initialsInput = document.querySelector('#initials');
+
+var questions = [
+    {
+        theQuestions: "question 1",
+        answerChoices: ["q1", "q2", "q3", "q4"],
+        correctAnswer: "answer 1"
+    },
+
+    {
+        theQuestions: "question 2",
+        answerChoices: ["q1", "q2", "q3", "q4"],
+        correctAnswer: "answer 2"
+    },
+
+    {
+        theQuestions: "question 3",
+        answerChoices: ["q1", "q2", "q3", "q4"],
+        correctAnswer: "answer 3"
+    },
+
+    {
+        theQuestions: "question 4",
+        answerChoices: ["q1", "q2", "q3", "q4"],
+        correctAnswer: "answer 4"
+    },
+
+    {
+        theQuestions: "question 5",
+        answerChoices: ["q1", "q2", "q3", "q4"],
+        correctAnswer: "answer 5"
+    },
+]
